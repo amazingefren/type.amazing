7c54f03477a13aab1cb5bf538d4a296f6c811286
amazingefren <efrentheroker@hotmail.com>
2021-09-29 20:30:06 -0600

    basic passport auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,15 @@
-// Lib
+// Module Imports
 import * as express from "express";
 import * as mongoose from "mongoose";
+import * as session from "express-session";
+import * as passport from "passport";
+import * as dotenv from 'dotenv'
+import { Strategy as LocalStrategy } from "passport-local";
+dotenv.config()
 
 // Local Imports
 import router from "./routes";
+import User from "./models/user.model";
 
 // Util Imports
 import getLogger from "./utils/logger";
@@ -12,10 +18,43 @@ const logger = getLogger("Server");
 // Constants
 const app = express();
 const port = process.env.PORT || 8000;
-const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/type";
+const dbUri = process.env.MONGO_URI || "mongodb://localhost:27017/type";
+
+// Middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "HELLOWORLD",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((id, done) => {
+  done(null, id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err: Error, user: typeof User) => done(err, user));
+});
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, {message: "User Not Found"})
+
+      //Bcrypt Here
+      if (password === user.password) {
+        return done(null, user)
+      }
+      return done(null,false,{message:"User Not Found 2!"})
+    });
+  })
+);
 
 // Express
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
 
--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,9 +1,25 @@
+import * as passport from "passport";
 import { Router } from "express";
+
+import User from "../../models/user.model";
+import { RegisterInput } from "./dto/user.dto";
+
+import log from "../../utils/logger";
+const logger = log("/auth");
 
 const router = Router();
 
-router.use("/", (_, res) => {
-  res.send("API ROUTE");
+router.get("/login", (req, res) => {
+  if (req.isAuthenticated()) return res.send("HII");
+  res.send("You're not logged in");
+});
+
+router.post("/login", passport.authenticate("local", { successRedirect: "/" }));
+
+router.post("/register", (req, res) => {
+  if (req.body.password) return res.send("hi");
+  logger.debug(JSON.stringify(req.body));
+  res.send(true);
 });
 
 export default router;
